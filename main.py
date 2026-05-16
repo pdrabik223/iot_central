7a7f62aa3b95465c4146e9f1b283011391ca78e2
pdrabik223 <pdrabik223@gmail.com>
2026-05-16 19:39:33 +0200

    LETS test in production

--- main.py
+++ main.py
@@ -54,117 +54,153 @@ def synch_time(rtc, timezone_offset=1):
 def home_page(cl: socket.socket, parameters: dict):
     cl.sendall(compose_response(response=load_html("static/index.html")))
 
+
 def add_redirect(cl: socket.socket, parameters: dict):
     global endpoint_config
     host_name = parameters.get("host_name", None)
     target = parameters.get("target", None)
-    
+
     if host_name is None:
-        return  cl.sendall(compose_response(status_code=400))
-    
+        return cl.sendall(compose_response(status_code=400))
+
     if target is None:
-        return  cl.sendall(compose_response(status_code=400))
-    
+        return cl.sendall(compose_response(status_code=400))
+
     for id, config in enumerate(endpoint_config):
-        
+
         if config.host_name == host_name:
             endpoint_config[id].redirects_to.append(target)
             break
-    
+
     print([data.to_dict() for data in endpoint_config])
-    
+
     safe_config()
-    
+
     cl.sendall(compose_response())
 
-# def validate_request(cl: socket.socket, 
-#                      parameters: dict, 
-#                      param_name:str, 
-#                      default_val:any = None, 
-#                      type:  type | any = any) -> None:
-    
-#     host_name = parameters.get(param_name, default_val)
-    
-#     if host_name is None:
-#         return  cl.sendall(compose_response(status_code=400))
 
 def remove_redirect(cl: socket.socket, parameters: dict):
     global endpoint_config
     host_name = parameters.get("host_name", None)
     target = parameters.get("target", None)
-    
+
     if host_name is None:
-        return  cl.sendall(compose_response(status_code=400))
-    
+        return cl.sendall(compose_response(status_code=400))
+
     if target is None:
-        return  cl.sendall(compose_response(status_code=400))
-    
+        return cl.sendall(compose_response(status_code=400))
+
     for id, config in enumerate(endpoint_config):
-        
+
         if config.host_name == host_name:
             endpoint_config[id].redirects_to.remove(target)
-    
+
     safe_config()
     cl.sendall(compose_response())
 
 
-def validate_required(cl: socket.socket, parameters: dict, param:str, type:type = any)->any:
+def delete_host(cl: socket.socket, parameters: dict):
+    global endpoint_config
+    param = "host_name"
     val = parameters.get(param, None)
-    
+
     if val is None:
-        return  cl.sendall(compose_response(status_code=400, response=f"missing param '{param}', of type: '{type}'"))
-    
+        return cl.sendall(
+            compose_response(
+                status_code=400, response=f"Missing param '{param}', of type: '{type}'"
+            )
+        )
+
+    if type(val) is not str:
+        return cl.sendall(
+            compose_response(
+                status_code=400,
+                response=f"Invalid '{param}' type, expected: '{type}', got: '{type(val)}'",
+            )
+        )
+
+    endpoint_config = [config for config in endpoint_config if config.host_name != val]
+
+    safe_config()
+    cl.sendall(compose_response())
+
+
+def validate_required(
+    cl: socket.socket, parameters: dict, param: str, type: type = any
+) -> any:
+    val = parameters.get(param, None)
+
+    if val is None:
+        return cl.sendall(
+            compose_response(
+                status_code=400, response=f"Missing param '{param}', of type: '{type}'"
+            )
+        )
+
     if type(val) is not type:
-        return  cl.sendall(compose_response(status_code=400, response=f"invalid '{param}' type, expected: '{type}', got: '{type(val)}'"))
-    
+        return cl.sendall(
+            compose_response(
+                status_code=400,
+                response=f"Invalid '{param}' type, expected: '{type}', got: '{type(val)}'",
+            )
+        )
+
     return val
-    
+
+
 def connect(cl: socket.socket, parameters: dict):
 
     host_name = validate_required(cl, parameters, "host_name", str)
     ip = validate_required(cl, parameters, "ip", str)
-    
+    redirect_endpoint = parameters.get("redirect_endpoint", None)
+
     is_new_connection = True
     requires_save = False
-    
+
     for id, config in enumerate(endpoint_config):
         if host_name != config.host_name:
             continue
-        
+
         if config.ip != ip:
             requires_save = True
 
-        config.ip = ip  
+        if config.redirect_endpoint != redirect_endpoint:
+            requires_save = True
+
+        config.ip = ip
+        config.redirect_endpoint = redirect_endpoint
         config.last_connection_time = utime.localtime()
+
         is_new_connection = False
-        
+
         for target in config.redirects_to:
             parameters_copy = parameters.copy()
-            
+
             url = f"{target}?"
             for val in parameters_copy:
                 url += f"{val}={parameters_copy[val]}"
-            
+
             try:
-                res = requests.get(url=url,timeout=3)
+                res = requests.get(url=url, timeout=3)
                 res.close()
                 gc.collect()
             except Exception as err:
                 print(err)
         break
-    
+
     if is_new_connection:
         endpoint_config.append(
             Endpoint(
                 parameters["host_name"],
                 utime.localtime(),
                 parameters.get("ip", "Unknown"),
+                redirect_endpoint,
             )
         )
 
     if is_new_connection or requires_save:
         safe_config()
-    
+
     cl.sendall(compose_response())
 
 
@@ -172,10 +208,11 @@ def get_endpoint_config(cl: socket.socket, parameters: dict):
     data = [endpoint.to_dict() for endpoint in endpoint_config]
     cl.sendall(compose_response(response=json.dumps(data)))
 
+
 def timestamp_to_str(t: tuple) -> str:
     def add_zero(val: int):
         return f"{val if val >= 10 else '0' + str(val)}"
-    
+
     return f"{add_zero(t[2])}/{add_zero(t[1])}/{add_zero(t[0])} {add_zero(t[3])}:{add_zero(t[4])}"
 
 
@@ -196,7 +233,9 @@ class Endpoint:
     last_connection_time: tuple
     redirects_to: list[str]
 
-    def __init__(self, host_name, last_connection_time, ip, redirect_endpoint, redirects_to=[]):
+    def __init__(
+        self, host_name, last_connection_time, ip, redirect_endpoint, redirects_to=[]
+    ):
         self.host_name = host_name
         self.ip = ip
         self.redirect_endpoint = redirect_endpoint
@@ -211,59 +250,65 @@ class Endpoint:
             "last_connection_time": timestamp_to_str(self.last_connection_time),
             "redirects_to": self.redirects_to,
         }
-        
+
     @staticmethod
-    def from_dict(data:dict):
+    def from_dict(data: dict):
         last_connection_time = data.get("last_connection_time")
-        
+
         if isinstance(last_connection_time, str):
             parsed = str_to_timestamp(last_connection_time)
             if parsed is not None:
                 last_connection_time = parsed
 
-        return Endpoint(host_name=data.get("host_name"),
-                        ip=data.get("ip"),
-                        redirect_endpoint=data.get("redirect_endpoint"),
-                        last_connection_time=last_connection_time,
-                        redirects_to=data.get("redirects_to"))
-        
+        return Endpoint(
+            host_name=data.get("host_name"),
+            ip=data.get("ip"),
+            redirect_endpoint=data.get("redirect_endpoint"),
+            last_connection_time=last_connection_time,
+            redirects_to=data.get("redirects_to"),
+        )
+
+
 def safe_config():
     global endpoint_config
     path = "endpoint_config.json"
     data = []
-    
+
     for config in endpoint_config:
         data.append(config.to_dict())
-    
-    write_json_file(path, data) 
+
+    write_json_file(path, data)
+
 
 def load_config():
     global endpoint_config
     path = "endpoint_config.json"
-    
+
     config = [Endpoint.from_dict(data) for data in load_json_file(path)]
-     
+
     if config is not None:
         endpoint_config = config
 
-def load_json_file(path:str)->dict|None:
+
+def load_json_file(path: str) -> dict | None:
     try:
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             return json.loads(file.read())
     except Exception:
         return None
-    
-def write_json_file(path:str, data:dict)->None:
+
+
+def write_json_file(path: str, data: dict) -> None:
     try:
-        with open(path, 'w') as file:
+        with open(path, "w") as file:
             file.write(json.dumps(data))
             file.flush()
     except Exception:
         return None
 
 
-
 import urequests as requests
+
 endpoint_config: list[Endpoint] = []
 
 
@@ -274,13 +319,14 @@ if __name__ == "__main__":
     _thread.start_new_thread(animation, ())
 
     load_config()
-    
+
     app.register_endpoint("/v1/", home_page)
     app.register_endpoint("/v1/get_endpoint_config", get_endpoint_config)
     app.register_endpoint("/v1/connect", connect)
+    app.register_endpoint("/v1/delete_host", delete_host)
     app.register_endpoint("/v1/add_redirect", add_redirect)
     app.register_endpoint("/v1/remove_redirect", remove_redirect)
-    
+
     try:
         app.main_loop()
     except (KeyboardInterrupt, Exception) as ex:
